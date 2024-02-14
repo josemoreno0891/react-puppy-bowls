677d3974d687343deda5485b81ec17045720b752
Jose Moreno <josemoreno0891@yahoo.com>
2024-02-14 14:22:16 -0500

    add fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import allplayers from './components/allplayers';
 import singlePlayer from './components/singlePlayer';
+import { getPlayers } from './api';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    getPlayers().then((players) => console.log(players));
+  }, [])
+  async function getAllPlayers() {
+    const players = await getPlayers();
+    console.log(players);
+  }
+getAllPlayers();
   return (
     <>
       <div>
-     Hi there
+      Hi there!
       </div>
     </>
   )
 }
-
 export default App
---
+++ src/api.js
@@ -0,0 +1,50 @@
+const API_URL = "https://fsa-puppy-bowl.herokuapp.com/api/2308-ac-pt-web-pt-a";
+export async function getPlayers(){
+    try {
+        const response = await fetch(`${API_URL}/players`);
+        const result = await response.json();
+        return result.data.players;
+
+      } catch (err) {
+        console.error(err);
+      }
+}
+
+export async function getPlayer(playerId){
+    try {
+        const response = await fetch(`${API_URL}/players/${playerId}`);
+        const result = await response.json();
+        return result.data.player;
+
+      } catch (err) {
+        console.error(err);
+      }
+}
+
+export async function createPlayer(playerId){
+    try {
+        const response = await fetch(`${API_URL}/players`,{
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(player)
+          });
+        const result = await response.json();
+        return result.data.newPlayer;
+      } catch (err) {
+        console.error(err);
+      }
+}
+
+export async function deletePlayer(playerId){
+    try {
+        const response = await fetch(`${API_URL}/players/${playerId}`,
+        {
+            method: "DELETE",
+        });
+        await response.json();
+      } catch (err) {
+        console.error(err);
+      }
+}
